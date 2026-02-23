051664e1c6ca55d15f6a052e2bdc9edff3b3bbf0
İsmail Umutluoğlu <ismailumutluoglu10@gmail.com>
2026-02-23 03:36:24 +0300

    frontend tasarımı yapıldı

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config(); //bu satır önemli .env içeriğini aktarmak için !!! 
 import cors from 'cors';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
 import routes from './routes/tasks.js'
 import connectDB from './db/connectDB.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleware
-app.use(express.static('./public'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.json());
 
 //Routes
